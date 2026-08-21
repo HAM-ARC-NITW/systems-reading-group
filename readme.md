a1a20c66ef6d4d52f7a6385771aa3cc61bc5b521
Mukesh <mukeshreddy.work@gmail.com>
2026-08-22 02:49:02 +0530

    oops

--- readme.md
+++
@@ -1,3 +0,0 @@
-# systems-reading-group
-
-a minimal website for HAM's systems reading group. built using [elm](https://elm-lang.org) for frontend and [typst](https://typst.app) for latex typesetting.
--- src/Utils.elm
+++
@@ -1,9 +0,0 @@
-module Utils exposing (link)
-
-import Html
-import Html.Attributes exposing (href, target)
-
-
-link : String -> String -> Html.Html msg
-link label url =
-    Html.a [ href url, target "_blank" ] [ Html.text label ]
